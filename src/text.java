fa211ca5a5c2df0795029550f3ded5205f70d033
ysr <1959166558@qq.com>
2024-07-17 10:28:17 +0800

    for循环输出不被5整除的偶数

--- src/text.java
+++ src/text.java
@@ -1,2 +1,15 @@
 public class text {
+    public static void main(String[] args) {
+        int i = 100;
+        int n = 0;
+        for(;i <= 999;i++){
+            if(i % 5 != 0 && i % 2 == 0) {
+                System.out.print(i + "\t");
+                n++;
+                if(n % 5 == 0) {
+                    System.out.println();
+                }
+            }
+        }
+    }
 }

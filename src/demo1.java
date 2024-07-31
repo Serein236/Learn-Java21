96d4d807e0589a76781286f2607466ce70b0412b
ysr <1959166558@qq.com>
2024-07-31 15:49:12 +0800

    方法调用练习

---
+++ src/demo1.java
@@ -0,0 +1,9 @@
+public class demo1 {
+    public static void main(String[] args) {
+        grilFriend();
+    }
+    public static void grilFriend(){
+        System.out.println("女");
+        System.out.println("18");
+    }
+}

70fbf8502fd89c0268070dbd74b5aa223b464d3f
ysr <1959166558@qq.com>
2024-07-31 15:29:14 +0800

    方法最基本

---
+++ src/fangfa.java
@@ -0,0 +1,11 @@
+public class fangfa {
+    public static void main(String[] args) {
+        playGame();
+        playGame();
+    }
+    public static void playGame(){
+        System.out.println("开始");
+        System.out.println("过程");
+        System.out.println("结束");
+    }
+}

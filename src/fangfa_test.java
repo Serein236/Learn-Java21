933e5728b5f87c430dca00821b396968e11319f2
ysr <1959166558@qq.com>
2024-07-31 15:49:41 +0800

    方法练习

--- src/fangfa_test.java
+++ src/fangfa_test.java
@@ -1,46 +1,15 @@
 public class fangfa_test {
     public static void main(String[] args) {
-        person Ming = new person();
-        Ming.setName("Ming");
-        Ming.setAge(20);
-        System.out.println(Ming.getName()+"   "+Ming.getAge());
-        person Hong = new person();
-        Hong.setNameAndAge("Hong",24);
-        System.out.println(Hong.getName()+"   "+Hong.getAge());
+       int [] arr = {1,2,3,4,5,6,7,8,9};
+       printArr(arr);
     }
-}
-
-class person{
-    private String name;
-    private int age;
-    private int brith;
-
-    public String getName() {
-        return this.name;
-    }
-    public int getAge() {
-        return this.age;
-    }
-    public void setName(String name) {
-        if(name == null || name.isEmpty()){
-            throw new IllegalArgumentException("Name cannot be null or empty");
+    public static void printArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if(i == arr.length-1){
+                System.out.print(arr[i]);
+            }else{
+                System.out.print(arr[i] + ", ");
+            }
         }
-        this.name = name;
-    }
-    public void setAge(int age) {
-        if (age < 0 || age > 100) {
-            throw new IllegalArgumentException("Age must be between 0 and 100");
-        }
-        this.age = age;
-    }
-    public void setNameAndAge(String name, int age) {
-        if(name == null || name.isEmpty()){
-            throw new IllegalArgumentException("Name cannot be null or empty");
-        }
-        if(age < 0 || age > 100){
-            throw new IllegalArgumentException("Age must be between 0 and 100");
-        }
-        this.name = name;
-        this.age = age;
     }
 }
